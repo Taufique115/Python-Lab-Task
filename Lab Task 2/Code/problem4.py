e90f50bee343645c7311b06abf3466e8b0cdada2
Mostafa Jaman Taufique <mostafataufique@gmail.com>
2026-06-20 19:54:38 +0600

    Fix Lab Task 2 folder

---
+++ Lab Task 2/Code/problem4.py
@@ -0,0 +1,11 @@
+
+ch = input("Enter a character: ")
+
+
+ch = ch.lower()
+
+
+if ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u':
+    print(ch, "is a Vowel")
+else:
+    print(ch, "is not a Vowel")
